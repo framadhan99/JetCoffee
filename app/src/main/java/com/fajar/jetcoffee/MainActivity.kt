750f4ecfbad5f55dde3d54e817c503c6ea2c831c
framadhan99 <fajarramadhan99704@gmail.com>
2023-12-01 10:31:50 +0700

    sectiontext

--- app/src/main/java/com/fajar/jetcoffee/MainActivity.kt
+++ app/src/main/java/com/fajar/jetcoffee/MainActivity.kt
@@ -3,14 +3,28 @@ package com.fajar.jetcoffee
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.fajar.jetcoffee.model.dummyCategory
+import com.fajar.jetcoffee.ui.components.CategoryItem
 import com.fajar.jetcoffee.ui.components.Search
+import com.fajar.jetcoffee.ui.components.SectionText
 import com.fajar.jetcoffee.ui.theme.JetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,14 +40,54 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp(){
-
+    Column{
+        Banner()
+        SectionText(stringResource(R.string.section_category))
+        CategoryRow()
+    }
 }
 
-@Preview
+
+
 @Composable
-fun JetCoffeeAppPreview(){
-    JetCoffeeTheme {
+fun Banner(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
         Search()
     }
 }
+
+
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+//        modifier = modifier.padding()
+    ) {
+        items(dummyCategory, key = { it.textCategory }) { category ->
+            CategoryItem(category)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun JetCoffeeAppPreview(){
+    JetCoffeeTheme {
+        JetCoffeeApp()
+
+    }
+}
+
+
 

09609f3747a3ad4800cc6fc0e9fe6d9a0db3aa82
framadhan99 <fajarramadhan99704@gmail.com>
2023-12-01 09:57:08 +0700

    searchbar

---
+++ app/src/main/java/com/fajar/jetcoffee/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.fajar.jetcoffee
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.fajar.jetcoffee.ui.components.Search
+import com.fajar.jetcoffee.ui.theme.JetCoffeeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetCoffeeTheme {
+                JetCoffeeApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun JetCoffeeApp(){
+
+}
+
+@Preview
+@Composable
+fun JetCoffeeAppPreview(){
+    JetCoffeeTheme {
+        Search()
+    }
+}
+
---
+++ app/src/main/java/com/fajar/jetcoffee/ui/theme/Color.kt
@@ -0,0 +1,6 @@
+package com.fajar.jetcoffee.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val CoffeeBrown = Color(0xFF886345)
+val LightCoffeeBrown = Color(0xFFC4A68A)
